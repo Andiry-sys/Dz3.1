c1f06678f53f9865297df79a2deef054d37150b4
Andriy-sys <marchycandriy@gmail.com>
2022-02-01 11:59:35 +0200

    HTML,CSS and JS

---
+++ Task1/app.js
@@ -0,0 +1,10 @@
+let jin = document.getElementById('in');
+let jout = document.getElementById('out');
+
+jin.addEventListener('keydown', function (e) {
+  if (e.key.match(/[0-9]/)) return e.preventDefault();
+});
+
+jin.addEventListener('input', function (e) {
+  jin.value = jin.value.replace(/[0-9]/g, '');
+});
